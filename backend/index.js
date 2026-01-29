04f36e8fa876d13f280398a09ed94d573789b5de
Mrpraviya <sandeeptha2001praveen@gmail.com>
2026-01-30 00:24:17 +0530

    MongoDB connectoin creation

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors";
- 
-// // dotenv.config();
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-
-// // MongoDB connection
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("✅ MongoDB connected"))
-//   .catch((err) => console.error(err));
-
-// // Routes
-// import authRoutes from "./routes/auth.js";
-// app.use("/api/auth", authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -36,7 +13,7 @@ const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/ims-db";
 mongoose
   .connect(mongoUri)
   .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 // Routes
 import authRoutes from "./routes/auth.js";
